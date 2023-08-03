7df4c57797e180f5be406c1e50e7d677657cb9a7
bifidokk <bifidokk@gmail.com>
2023-08-03 20:18:38 +0300

    Add payment domain service

--- payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/entity/CreditHistory.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/entity/CreditHistory.java
@@ -31,6 +31,10 @@ public class CreditHistory extends BaseEntity<CreditHistoryId> {
         return transactionType;
     }
 
+    public static Builder builder() {
+        return new Builder();
+    }
+
     public static final class Builder {
         private CreditHistoryId creditHistoryId;
         private CustomerId customerId;
@@ -40,9 +44,7 @@ public class CreditHistory extends BaseEntity<CreditHistoryId> {
         private Builder() {
         }
 
-        public static Builder builder() {
-            return new Builder();
-        }
+
 
         public Builder creditHistoryId(CreditHistoryId val) {
             creditHistoryId = val;
